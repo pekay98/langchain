a21836487a2116a26521498adce5dc9799e14fd5
pekay98 <prana98khatri@gmail.com>
2026-05-05 02:48:26 +0530

    updated the search-agent

---
+++ main.py
@@ -0,0 +1,38 @@
+from dotenv import load_dotenv
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+from tavily import TavilyClient
+import os
+
+
+load_dotenv()
+
+@tool
+def search(query: str) -> str:
+    """Search the web for a query"""
+    print(f"\n\nSearching for {query} \n\n")
+    tavily = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+
+    return tavily.search(query=query)
+
+tools = [search, TavilySearch()]
+llm = ChatOpenAI()
+agent = create_agent(
+    model=llm,
+    tools=tools
+)
+
+
+def main(): 
+    print("Hello from langchain-course!")
+    response = agent.invoke({
+        "messages":HumanMessage(content="Search for job openeings in India with 1 year experience and skill as AI Engineer and Langchain")
+    })
+    print(response)
+
+
+if __name__ == "__main__":
+    main()
